024a1281ceb86bd27d8c55f9381e7a83720242c0
zeeshanshaikh001 <zeeshanshaikhonline@gmail.com>
2019-08-08 02:42:36 +0530

    commit for multiply method

--- test/java/com/qa/tests/TestBase.java
+++ test/java/com/qa/tests/TestBase.java
@@ -4,9 +4,24 @@ import org.testng.annotations.Test;
 
 public class TestBase {
 	
-	@Test
+	@Test(priority=1)
 	public void firstTestcase() {
 		System.out.println("Hello TestNG");
+	}
+	
+
+	public int multiply(int num1, int num2) {
+	int	mul = num1*num2;
+	return mul;
+	}
+	
+	@Test(priority=2)
+	public void testMultiply() {
+		TestBase TestBase = new TestBase();
+		int a=2;
+		int b=3;
+		int mulres = TestBase.multiply(a, b);
+		System.out.println("Multiplication of " +a + " and " +b+ " is " + mulres);
 	}
 
 }
